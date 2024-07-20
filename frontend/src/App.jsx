0792880b7590fa1367ac868e521271723489f16d
Vinay S <vinayshankar0369@gmail.com>
2024-07-21 03:16:28 +0530

    added passwords for security

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+// const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://filetransfer-111n.onrender.com'
+
 function App() {
   const [files, setFiles] = useState([]);
   const [emails, setEmails] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleFileChange = (event) => {
     setFiles(event.target.files);
@@ -12,6 +16,10 @@ function App() {
 
   const handleEmailsChange = (event) => {
     setEmails(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
   const handleSubmit = async (event) => {
@@ -25,9 +33,10 @@ function App() {
     // Convert emails string to an array
     const emailArray = emails.split(',').map(email => email.trim());
     formData.append('emails', JSON.stringify(emailArray));
+    formData.append('password', password); // Add the password to the form data
 
     try {
-      const response = await axios.post('https://filetransfer-111n.onrender.com/upload', formData, {
+      const response = await axios.post(`${BASE_URL}/upload`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -36,6 +45,7 @@ function App() {
       alert(response.data.message);
       setFiles([]);
       setEmails('');
+      setPassword('');
       event.target.reset();
     } catch (error) {
       console.error('Error:', error);
@@ -60,6 +70,14 @@ function App() {
           placeholder="Enter email addresses, separated by commas"
           value={emails}
           onChange={handleEmailsChange}
+          required
+        />
+        <input
+          type="password"
+          className="input-password"
+          placeholder="Enter a password for the zip file"
+          value={password}
+          onChange={handlePasswordChange}
           required
         />
         <button type="submit" className="submit-button">Upload Files</button>
